02db90528ddb834046ebb8a1b7d38b65318b38b7
Rachel Frederickson <vampirerachel@gmail.com>
2024-01-25 22:22:19 -0500

    box working

--- my-portfolio-app/src/components/body/ColorBox.jsx
+++ my-portfolio-app/src/components/body/ColorBox.jsx
@@ -1,27 +1,79 @@
-import './Colorbox.scss';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import './ColorBox.scss'
 
-const Colorbox = ({ colors }) => {
+const ColorBox = () => {
+  const { t } = useTranslation();
+
+  const [boxColors, setBoxColors] = useState({
+    base: '#2e1f1f',
+    second: '#39404a',
+    third: '#706b6b',
+    fourth: '#394f5a',
+    fifth: '#9e7f7f',
+    sixth: '#b9a099',
+    seventh: '#e7c9c9',
+    eighth: '#e1dcdc',
+    ninth: '#d2d2d2',
+    tenth: '#c5c1c1',
+    hover: '#ad9384',
+    navBackground: '#b59797',
+    navText: '#070707',
+  });
+
+  useEffect(() => {
+    console.log('Initial state:', boxColors);
+  }, []); // Run only once on mount
+
+  const handleClick = () => {
+    setBoxColors((prevColors) => {
+      const newColors = {
+        base: getRandomColor(),
+        second: getRandomColor(),
+        third: getRandomColor(),
+        fourth: getRandomColor(),
+        fifth: getRandomColor(),
+        sixth: getRandomColor(),
+        seventh: getRandomColor(),
+        eighth: getRandomColor(),
+        ninth: getRandomColor(),
+        tenth: getRandomColor(),
+        hover: getRandomColor(),
+        navBackground: getRandomColor(),
+        navText: getRandomColor(),
+      };
+
+      console.log('New state:', newColors);
+      return newColors;
+    });
+  };
+
+  const getRandomColor = () => {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  };
+
   return (
-    <div className="cbox-container">
-      {colors.map((color, index) => (
-        <div key={`${index}-cbox`} className="cbox-layered-cbox" style={{ backgroundColor: color }}>
-          <div className="cbox-base-layer"></div>
-          <div className="cbox-top-layer"></div>
-          <div className="cbox-third-layer"></div>
-          <div className="cbox-fourth-layer"></div>
-          <div className="cbox-fifth-layer"></div>
-          <div className="cbox-sixth-layer"></div>
-          <div className="cbox-seventh-layer"></div>
-          <div className="cbox-eighth-layer"></div>
-          <div className="cbox-ninth-layer"></div>
-          <div className="cbox-tenth-layer"></div>
-          <div className="cbox-content-box">
-            <p>{color}</p>
-          </div>
+    <div className="box-container">
+      <div className="box-layered-box">
+        <div className="box-base-layer" style={{ backgroundColor: boxColors.base }}></div>
+        <div className="box-top-layer" style={{ backgroundColor: boxColors.hover }}></div>
+        <div className="box-third-layer" style={{ backgroundColor: boxColors.third }}></div>
+        <div className="box-fourth-layer"style={{ backgroundColor: boxColors.fourth }}></div>
+        <div className="box-fifth-layer"style={{ backgroundColor: boxColors.fifth }}></div>
+        <div className="box-sixth-layer"style={{ backgroundColor: boxColors.sixth }}></div>
+        <div className="box-seventh-layer"style={{ backgroundColor: boxColors.seventh }}></div>
+        <div className="box-eighth-layer"style={{ backgroundColor: boxColors.eighth }}></div>
+        <div className="box-ninth-layer"style={{ backgroundColor: boxColors.ninth }}></div>
+        <div className="box-tenth-layer"style={{ backgroundColor: boxColors.tenth}}></div>
+        <div className="box-content-box" style={{ backgroundColor: boxColors.hover }}>
+          <p>{t('box.1')}</p>
         </div>
-      ))}
+      </div>
+      <button type="button" className="change-color" onClick={handleClick}>
+        Change color
+      </button>
     </div>
   );
 };
 
-export default Colorbox;
+export default ColorBox;
--- my-portfolio-app/src/components/webdev/ColorPicker.jsx
+++ my-portfolio-app/src/components/webdev/ColorPicker.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react';
+
 import './colorpicker.scss';
-import Colorbox from '../body/ColorBox';
+import ColorBox from '../body/ColorBox';
 
 const ColorPicker = () => {
-  const [boxColors, setBoxColors] = useState(Array(10).fill(getRandomHexColor()));
 
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-  }
 
-  const handleClick = () => {
-    const newColors = Array(10).fill().map(() => getRandomHexColor());
-    setBoxColors(newColors);
-  };
 
   return (
     <div className="colorpicker-container">
@@ -22,12 +14,9 @@ const ColorPicker = () => {
           this was so fun to learn how to generate random colors and their hex-codes to find inspiration when choosing
           a color palette: 
         </p>
-        <button type="button" className="change-color" onClick={handleClick}>
-          Change color
-        </button>
       </div>
       <div className="colorbox-container">
-        <Colorbox colors={boxColors} />
+        <ColorBox />
       </div>
     </div>
   );
